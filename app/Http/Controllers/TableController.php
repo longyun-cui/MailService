45e7a22a09776af88af4a5eba0f54d10b89302f6
longyun <longyun@longyundeMacBook-Pro.local>
2018-02-06 16:51:20 +0800

    test

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -23,10 +23,10 @@ class TableController extends Controller
         $post_data['user_id'] = 1;
         $post_data['code'] = 1;
         $post_data['target'] = 'longyun-cui@163.com';
-        dd($post_data);
+//        dd($post_data);
 
-//        $flag = $this->repo->send_email_activation($post_data);
-//        dd($flag);
+        $flag = $this->repo->send_email_activation($post_data);
+        dd($flag);
     }
 
     public function send_email_activation()
